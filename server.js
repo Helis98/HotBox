078b5b64a8672e02363b519a2453019a308d96b5
Chaitanya <cvemuri06@gmail.com>
2021-05-06 11:47:27 -0400

    Server.js file now runs the actual server instead of crashing

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const mongoose = require('mongoose');
+
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); // Make sure it comes back as json
 
-app.use((req, res, next) => 
-{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
-});
 
-app.listen(5000); // start Node + Express server on port 5000
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb+srv://admin:prdqn1yLOCv3QsCf@hotboxcluster.e47sf.mongodb.net/HotBoxDB?retryWrites=true&w=majority');
+
+app.listen(3000, () => { console.log('Server is running...') }); //Listen on port 3000
