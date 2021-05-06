d673fd6fb98bf4c5806d1905a4cb38d2bdb3db5a
Chaitanya <cvemuri06@gmail.com>
2021-05-06 11:56:34 -0400

    Changed server.js file to use env file for DB url

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json()); // Make sure it comes back as json
 
+require('dotenv').config()
+const uri = process.env.MONGO_URL
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb+srv://admin:prdqn1yLOCv3QsCf@hotboxcluster.e47sf.mongodb.net/HotBoxDB?retryWrites=true&w=majority');
+mongoose.connect(uri);
 
 app.listen(3000, () => { console.log('Server is running...') }); //Listen on port 3000
