99f843077e4401737ee7f3f24a50356478b06232
Chaitanya <cvemuri06@gmail.com>
2021-05-26 13:04:07 -0400

    Created boxstatus updating and updated BoxSchema

--- models/BoxSchema.js
+++ models/BoxSchema.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const boxSchema = new mongoose.Schema({
     orderNumber: {
         type: Number,
-        required: true
     },
 
     Empty: {
         type: Boolean,
-        required: true
+        default: true
+    },
+
+    BoxID: {
+        type: String
+    },
+
+    BoxNumber: {
+        type: Number
     }
 
     
 });
 
+boxSchema.plugin(AutoIncrement, {inc_field: 'BoxNumber'});
 const Box = mongoose.model("Box", boxSchema);
 module.exports = Box;
--- routes/boxRoutes.js
+++ routes/boxRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const boxModel = require('../models/BoxSchema');
+const crypto = require("crypto");
 const bodyParser = require('body-parser');
 const { db } = require('../models/BoxSchema');
 const app = express();
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.post("/addbox", async (req, res) => {
+    req.body.BoxID = crypto.randomBytes(8).toString('hex');
     const Box = new boxModel(req.body);
     
     try {
@@ -19,12 +21,48 @@ app.post("/addbox", async (req, res) => {
       }
 });
 
-/*app.patch("/boxstatus", async (req, res) => {
-    const updateStatus = req.body;
-    try {
-        
+app.get("/getorder", async (req,res) => {
+    const Box = await boxModel.find({});
+    try{
+      res.send(Box);
+    }catch(err){
+      res.status(500).send(err);
+    }
+    
+});
+
+
+
+/*app.delete("/deletebox", async (req,res) => {
+    const Box = await boxModel.find({});
+    Box
+})*/
+
+app.patch("/boxstatus", async (req, res) => {
+    const id = req.body.BoxID;
+    //const updateStatus = req.body.status;
+    const update = {Empty: req.body.status};
+
+    const box = await boxModel.findOneAndUpdate(id, update);
+    /*try{
+      var box = await boxModel.find({BoxID : id});
+    }catch(err){
+      res.status(500).send("died looking");
     }
 
-})
-*/
+    try{
+      box.Empty = updateStatus;
+    }catch(err){
+      res.status(500).send("died updating");
+    }*/
+    
+    try {
+        await box.save();
+        res.send(box.Empty);
+    } catch(err){
+      res.status(500).send(err);
+    }
+
+});
+
 module.exports = app
