3dc7037a298e3c2cd56a6ba93d16aa20cec9cf04
Chaitanya <cvemuri06@gmail.com>
2021-05-28 00:25:04 -0400

    Fixing port stuff

--- server.js
+++ server.js
@@ -15,4 +15,6 @@ mongoose.connect(uri);
 
 app.use(boxRouter);
 
-app.listen(3000, () => { console.log('Server is running...') }); //Listen on port 3000
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => { console.log('Server is running...') }); //Listen on port 3000
