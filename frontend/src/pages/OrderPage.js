2304978d8016ac1e8336412dc0a4088b2348456a
AustinTillotson <austintillotson@knights.ucf.edu>
2021-06-24 14:58:49 -0400

    Added temp form for Order page

--- frontend/src/pages/OrderPage.js
+++ frontend/src/pages/OrderPage.js
@@ -1,5 +1,14 @@
+import classes from "./OrderPage.module.css";
+import OrderForm from "../components/forms/OrderForm";
+
 function OrderPage() {
-  return <div>OrderPage</div>;
+  return (
+    <div className={classes.main}>
+      <div className={classes.container}>
+        <OrderForm />
+      </div>
+    </div>
+  );
 }
 
 export default OrderPage;
