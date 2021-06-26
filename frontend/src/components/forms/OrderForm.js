cbfe847933fcf66ba17ed04d386970182170f023
AustinTillotson <austintillotson@knights.ucf.edu>
2021-06-26 18:25:18 -0400

    Adjust inputs

--- frontend/src/components/forms/OrderForm.js
+++ frontend/src/components/forms/OrderForm.js
@@ -3,21 +3,21 @@ import { useRef } from "react";
 import classes from "./AddBoxForm.module.css";
 
 function OrderForm() {
+  const nameRef = useRef();
   const foodRef = useRef();
   const tempRef = useRef();
-  const timeRef = useRef();
 
   function submitHandler(event) {
     event.preventDefault();
 
+    const name = nameRef.current.value;
     const food = foodRef.current.value;
     const temp = tempRef.current.value;
-    const time = timeRef.current.value;
 
     const orderData = {
+      name: name,
       food: food,
       temp: temp,
-      time: time,
     };
 
     console.log(orderData);
@@ -27,16 +27,16 @@ function OrderForm() {
     <div>
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.input}>
-          <label htmlFor="food">Food Choice</label>
+          <label htmlFor="name">Enter Name</label>
+          <input type="text" required id="name" ref={nameRef} />
+        </div>
+        <div className={classes.input}>
+          <label htmlFor="food">Select Food</label>
           <input type="text" required id="food" ref={foodRef} />
         </div>
         <div className={classes.input}>
-          <label htmlFor="temp">Select Temperature</label>
+          <label htmlFor="temp">Select Temperature (optional add)</label>
           <input type="text" required id="temp" ref={tempRef} />
-        </div>
-        <div className={classes.input}>
-          <label htmlFor="time">Time</label>
-          <input type="text" required id="time" ref={timeRef} />
         </div>
         <div className={classes.button}>
           <button className="btn">Submit Order</button>
