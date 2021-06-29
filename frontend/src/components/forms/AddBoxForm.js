a662ffc68013c6ce74651213268a93015a0ec15b
AustinTillotson <austintillotson@knights.ucf.edu>
2021-06-29 14:33:41 -0400

    Initial implementation of addBox
    Code segments have been left and commented out for future reference

--- frontend/src/components/forms/AddBoxForm.js
+++ frontend/src/components/forms/AddBoxForm.js
@@ -3,47 +3,64 @@ import { useRef } from "react";
 import classes from "./AddBoxForm.module.css";
 
 function AddBoxForm() {
-  const boxidRef = useRef();
-  const statusRef = useRef();
-  const infoRef = useRef();
+  //const boxidRef = useRef();
+  //const statusRef = useRef();
+  //const infoRef = useRef();
 
-  function submitHandler(event) {
+  //function submitHandler(event) {
+  function addBox(event) {
     event.preventDefault();
 
-    const boxid = boxidRef.current.value;
+    /*  const boxid = boxidRef.current.value;
     const status = statusRef.current.value;
     const info = infoRef.current.value;
 
     const boxData = {
-      id: boxid,
-      status: status,
-      info: info,
-    };
+      BoxNumber: boxid,
+      Empty: status,
+    };*/
 
-    console.log(boxData);
+    try {
+      //console.log(boxData);
+
+      fetch("http://localhost:5000/addbox", {
+        method: "POST",
+        body: JSON.stringify(/*boxData*/),
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (err) {
+      alert(err.toString());
+      return;
+    }
   }
 
   return (
     <div>
-      <form className={classes.form} onSubmit={submitHandler}>
-        <div className={classes.input}>
-          <label htmlFor="boxid">Box ID</label>
-          <input type="text" required id="boxid" ref={boxidRef} />
-        </div>
-        <div className={classes.input}>
-          <label htmlFor="status">Status</label>
-          <input type="text" required id="status" ref={statusRef} />
-        </div>
-        <div className={classes.input}>
-          <label htmlFor="info">Info</label>
-          <input type="text" required id="info" ref={infoRef} />
-        </div>
-        <div className={classes.button}>
-          <button className="btn">Add Box</button>
-        </div>
-      </form>
+      <div className={classes.button}>
+        <button className="btn" onClick={addBox}>Add Box</button>
+      </div>
     </div>
   );
 }
 
 export default AddBoxForm;
+
+/*
+<form className={classes.form} onSubmit={submitHandler}>
+<div className={classes.input}>
+  <label htmlFor="boxid">Box ID</label>
+  <input type="text" required id="boxid" ref={boxidRef} />
+</div>
+<div className={classes.input}>
+  <label htmlFor="status">Status</label>
+  <input type="text" required id="status" ref={statusRef} />
+</div>
+<div className={classes.input}>
+  <label htmlFor="info">Info</label>
+  <input type="text" required id="info" ref={infoRef} />
+</div>
+<div className={classes.button}>
+  <button className="btn">Add Box</button>
+</div>
+</form>
+*/
