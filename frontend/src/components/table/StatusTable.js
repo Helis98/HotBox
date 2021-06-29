d09a969ef9e356a619c11f0d02d4bbdef07c225e
AustinTillotson <austintillotson@knights.ucf.edu>
2021-06-29 14:33:05 -0400

    Initial implementation of getorder
    Code segments have been left and commented out for future reference

--- frontend/src/components/table/StatusTable.js
+++ frontend/src/components/table/StatusTable.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import classes from "./StatusTable.module.css";
 import StatusTableHeader from "./StatusTableHeader";
 import StatusTableEntry from "./StatusTableEntry";
@@ -11,19 +13,61 @@ const TEST_DATA = [
 ];
 
 function StatusTable(props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedData, setLoadedData] = useState([]);
+
+  /*useEffect(() => {
+    setIsLoading(true);
+    fetch("http://localhost:5000/getorder")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {/*
+        const boxes = [];
+
+        for (const key in data) {
+          const box = {
+            id: key,
+            ...data[key]
+          };
+
+          boxes.push(box);
+        }//
+        setIsLoading(false);
+        setLoadedData(data);
+      });
+  }, []);*/
+
+  /* if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }*/
+
+  useEffect(() => {
+    fetch("http://localhost:5000/getorder")
+      .then((response) => response.json())
+      .then((data) => {
+        setLoadedData(data);
+        console.log(loadedData);
+      });
+  }, []);
+
   return (
     <div className={classes.main}>
       <ul>
         <li key="tableheader">
           <StatusTableHeader />
         </li>
-        {TEST_DATA.map((entry) => {
+        {loadedData.map((entry) => {
           return (
             <li key={entry.BoxID}>
               <StatusTableEntry
                 BoxNumber={entry.BoxNumber}
                 Empty={entry.Empty}
-                info={'todo'}
+                info={"todo"}
               />
             </li>
           );
