7598561f2a91ee5d50ce0f99f01dc7f335c4947f
AustinTillotson <austintillotson@knights.ucf.edu>
2021-07-01 12:16:21 -0400

    Cleanup

--- frontend/src/components/forms/OrderForm.js
+++ frontend/src/components/forms/OrderForm.js
@@ -3,26 +3,14 @@ import { useRef } from "react";
 import classes from "./OrderForm.module.css";
 
 function OrderForm() {
-  //const nameRef = useRef();
-  //const foodRef = useRef();
-  //const tempRef = useRef();
   const boxIDRef = useRef();
   const orderNumRef = useRef();
 
   function submitHandler(event) {
     event.preventDefault();
 
-    //const name = nameRef.current.value;
-    //const food = foodRef.current.value;
-    //const temp = tempRef.current.value;
     const boxID = boxIDRef.current.value;
     const orderNum = orderNumRef.current.value;
-
-    /*  const orderData = {
-      name: name,
-      food: food,
-      temp: temp,
-    };*/
 
     const orderData = {
       BoxID: boxID,
@@ -62,19 +50,3 @@ function OrderForm() {
 }
 
 export default OrderForm;
-
-/*
-
-        <div className={classes.input}>
-          <label htmlFor="name">Enter Name</label>
-          <input type="text" required id="name" ref={nameRef} />
-        </div>
-        <div className={classes.input}>
-          <label htmlFor="food">Select Food</label>
-          <input type="text" required id="food" ref={foodRef} />
-        </div>
-        <div className={classes.input}>
-          <label htmlFor="temp">Select Temperature (optional add)</label>
-          <input type="text" required id="temp" ref={tempRef} />
-        </div>
-*/
--- frontend/src/components/modals/AdminModal.js
+++ frontend/src/components/modals/AdminModal.js
@@ -3,10 +3,6 @@ import { useRef } from "react";
 import classes from "./AdminModal.module.css";
 
 function AddBoxForm() {
-  //const boxidRef = useRef();
-  //const statusRef = useRef();
-  //const infoRef = useRef();
-
   const boxidRef = useRef();
 
   function deleteBoxHandler(event) {
@@ -17,8 +13,6 @@ function AddBoxForm() {
     console.log("Delete" + boxid);
 
     try {
-      //console.log(boxData);
-
       fetch("http://localhost:5000/deletebox", {
         method: "DELETE",
         body: boxid,
@@ -32,21 +26,9 @@ function AddBoxForm() {
   function addBox(event) {
     event.preventDefault();
 
-    /*  const boxid = boxidRef.current.value;
-    const status = statusRef.current.value;
-    const info = infoRef.current.value;
-
-    const boxData = {
-      BoxNumber: boxid,
-      Empty: status,
-    };*/
-
     try {
-      //console.log(boxData);
-
       fetch("http://localhost:5000/addbox", {
         method: "POST",
-        body: JSON.stringify(/*boxData*/),
         headers: { "Content-Type": "application/json" },
       });
     } catch (err) {
@@ -76,23 +58,3 @@ function AddBoxForm() {
 }
 
 export default AddBoxForm;
-
-/*
-<form className={classes.form} onSubmit={submitHandler}>
-<div className={classes.input}>
-  <label htmlFor="boxid">Box ID</label>
-  <input type="text" required id="boxid" ref={boxidRef} />
-</div>
-<div className={classes.input}>
-  <label htmlFor="status">Status</label>
-  <input type="text" required id="status" ref={statusRef} />
-</div>
-<div className={classes.input}>
-  <label htmlFor="info">Info</label>
-  <input type="text" required id="info" ref={infoRef} />
-</div>
-<div className={classes.button}>
-  <button className="btn">Add Box</button>
-</div>
-</form>
-*/
