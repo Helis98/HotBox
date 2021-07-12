995a1b2a66fc7a02ae2cce8e6613ae4c35aa56df
AustinTillotson <austintillotson@knights.ucf.edu>
2021-07-12 17:17:13 -0400

    Added more fields for order

--- frontend/src/components/forms/OrderForm.js
+++ frontend/src/components/forms/OrderForm.js
@@ -13,12 +13,14 @@ function OrderForm() {
 
   const boxIDRef = useRef();
   const orderNumRef = useRef();
+  const foodRef = useRef();
 
   function submitHandler(event) {
     event.preventDefault();
 
     const boxID = boxIDRef.current.value;
     const orderNum = orderNumRef.current.value;
+    const food = foodRef.current.value;
 
     const orderData = {
       BoxID: boxID,
@@ -26,6 +28,7 @@ function OrderForm() {
     };
 
     console.log(orderData);
+    console.log(food);
     try {
       fetch(fetchPath("giveorder"), {
         method: "PATCH",
@@ -41,6 +44,24 @@ function OrderForm() {
   return (
     <div>
       <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.row}>
+          <div className={classes.input}>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" required id="firstName" />
+          </div>
+          <div className={classes.input}>
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" required id="lastName" />
+          </div>
+        </div>
+        <div className={classes.input}>
+          <label htmlFor="email">Email</label>
+          <input type="text" required id="email" />
+        </div>
+        <div className={classes.input}>
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input type="text" required id="phoneNumber" />
+        </div>
         <div className={classes.input}>
           <label htmlFor="boxid">Box ID</label>
           <input type="text" required id="boxid" ref={boxIDRef} />
@@ -48,6 +69,14 @@ function OrderForm() {
         <div className={classes.input}>
           <label htmlFor="ordernum">Order Number</label>
           <input type="text" required id="ordernum" ref={orderNumRef} />
+        </div>
+        <div className={classes.input}>
+          <label htmlFor="food">Select Your Food</label>
+          <select required id="food" ref={foodRef}>
+            <option value="bacon">Bacon</option>
+            <option value="burger">Burger</option>
+            <option value="wrap">Wrap</option>
+          </select>
         </div>
         <div className={classes.button}>
           <button className="btn">Submit Order</button>
