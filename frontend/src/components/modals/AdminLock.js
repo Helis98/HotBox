a3d8efd45c62ca6a9e9edc9abf919ee230a1b5d2
Chaitanya <cvemuri06@gmail.com>
2021-07-15 18:38:28 -0400

    Adding changes to backend for api connection

---
+++ frontend/src/components/modals/AdminLock.js
@@ -0,0 +1,65 @@
+import { useState, useRef } from "react";
+
+import classes from "./AdminLock.module.css";
+import AdminModal from "./AdminModal";
+
+function ServerPage() {
+  function fetchPath(command) {
+    if (process.env.NODE_ENV === "production") {
+      return "https://hotbox14.herokuapp.com/" + command;
+    } else {
+      return "http://localhost:5000/" + command;
+    }
+  }
+
+  const [addModalOpen, setAddModalOpen] = useState(false);
+  const pinRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const pin = pinRef.current.value;
+    const data = {
+      code: pin,
+    };
+
+    try {
+      fetch(fetchPath("getcode"), {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      }).then((response) => {
+        if (response.status === 200) {
+          setAddModalOpen(true);
+        } else if (response.status === 406) {
+          console.log("Invalid code");
+        } else {
+          console.log("Error");
+        }
+      });
+    } catch (err) {
+      alert(err.toString());
+      return;
+    }
+  }
+  return (
+    <div className={classes.main}>
+      {!addModalOpen ? (
+        <form className={classes.form} onSubmit={submitHandler}>
+          <div className={classes.input}>
+            <label htmlFor="pin">Admin PIN</label>
+            <input type="password" required id="pin" ref={pinRef} />
+            <button className="btn">Submit Pin</button>
+          </div>
+        </form>
+      ) : null}
+      {addModalOpen ? (
+        <div className={classes.container}>
+          <AdminModal />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default ServerPage;
--- frontend/src/pages/StatusPage.js
+++ frontend/src/pages/StatusPage.js
@@ -1,9 +1,11 @@
 import StatusTable from "../components/table/StatusTable";
+import AdminLock from "../components/modals/AdminLock";
 
 function StatusPage() {
   return (
     <div>
       <StatusTable />
+      <AdminLock />
     </div>
   );
 }
