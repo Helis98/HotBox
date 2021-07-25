21caf8d98410765f41b8ef2f9dc83001660d34e5
AustinTillotson <austintillotson@knights.ucf.edu>
2021-07-24 22:59:07 -0400

    Implemented LockBoxModal into Admin

--- frontend/src/components/modals/AdminModal.js
+++ frontend/src/components/modals/AdminModal.js
@@ -4,10 +4,12 @@ import classes from "./AdminModal.module.css";
 import AddBoxModal from "./AddBoxModal";
 import BackdropModal from "./BackdropModal";
 import DeleteBoxModal from "./DeleteBoxModal";
+import LockBoxModal from "./LockBoxModal";
 
 function AddBoxForm() {
   const [displayAddModal, setDisplayAddModal] = useState(false);
   const [displayDeleteModal, setDisplayDeleteModal] = useState(false);
+  const [displayLockModal, setDisplayLockModal] = useState(false);
 
   function addBox() {
     setDisplayAddModal(true);
@@ -25,6 +27,14 @@ function AddBoxForm() {
     setDisplayDeleteModal(false);
   }
 
+  function lockBox() {
+    setDisplayLockModal(true);
+  }
+
+  function closeLockModal() {
+    setDisplayLockModal(false);
+  }
+
   function refreshPage() {
     window.location.reload(false);
   }
@@ -38,14 +48,19 @@ function AddBoxForm() {
         <button className="btn btnSpace" onClick={deleteBox}>
           Delete Box
         </button>
+        <button className="btn btn--alt btnSpace" onClick={lockBox}>
+          Lock Box
+        </button>
         <button className="btn btn--alt btnSpace" onClick={refreshPage}>
           Close Admin
         </button>
       </div>
       {displayAddModal && <AddBoxModal onCancel={closeAddModal} />}
       {displayDeleteModal && <DeleteBoxModal onCancel={closeDeleteModal} />}
+      {displayLockModal && <LockBoxModal onCancel={closeLockModal} />}
       {displayAddModal && <BackdropModal onClick={closeAddModal} />}
       {displayDeleteModal && <BackdropModal onClick={closeDeleteModal} />}
+      {displayLockModal && <BackdropModal onClick={closeLockModal} />}
     </div>
   );
 }
